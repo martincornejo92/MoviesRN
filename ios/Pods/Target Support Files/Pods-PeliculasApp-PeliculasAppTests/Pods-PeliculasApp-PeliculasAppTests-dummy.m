2165d247d859420b3dfc7edb958c5c51bbfd1c70
Martin Cornejo <martincornejo@MacBook-Air-de-Martin.local>
2023-04-13 10:41:31 -0300

    add proyect

---
+++ ios/Pods/Target Support Files/Pods-PeliculasApp-PeliculasAppTests/Pods-PeliculasApp-PeliculasAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PeliculasApp_PeliculasAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_PeliculasApp_PeliculasAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-PeliculasApp-tvOSTests/Pods-PeliculasApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PeliculasApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_PeliculasApp_tvOSTests
+@end
